68a022fe54ced56edf780737ce27dbd62518ecee
fabien <fabien.lierville27@gmail.com>
2022-02-21 11:51:58 +0100

    SharedPreferences

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
       ),
       home: const PageHome(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
--- lib/pages/page_home.dart
+++ lib/pages/page_home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class PageHome extends StatefulWidget {
   const PageHome({Key? key}) : super(key: key);
@@ -8,13 +9,60 @@ class PageHome extends StatefulWidget {
 }
 
 class _PageHomeState extends State<PageHome> {
+  List<String> villes = [];
+
+  @override
+  void initState() {
+    obtenir();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Météo"),
       ),
-      body: Center(),
+      body: Center(
+        child: ElevatedButton(
+          child: Text("Ajouter Ville"),
+          onPressed: (){
+            print(villes);
+            ajouter("Paris");
+            ajouter("Lyon");
+            supprimer("Rouen");
+            print(villes);
+          },
+        ),
+      ),
     );
+  }
+
+  Future<void> obtenir() async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String>? villesList = prefs.getStringList("villes");
+    if(villesList !=null){
+      setState(() {
+        villes = villesList;
+      });
+    }
+  }
+
+  Future<void> ajouter(String value) async{
+    //AntiDoublon
+    if(villes.contains(value)){
+      return;
+    }
+    villes.add(value);
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setStringList("villes", villes);
+    obtenir();
+  }
+
+  Future<void> supprimer(String value) async{
+    villes.remove(value);
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setStringList("villes", villes);
+    obtenir();
   }
 }
