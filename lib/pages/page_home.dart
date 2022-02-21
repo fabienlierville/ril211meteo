2e0156a3f4b56a0463508cfeb98c85dc70c109a7
fabien <fabien.lierville27@gmail.com>
2022-02-21 11:13:10 +0100

    first

---
+++ lib/pages/page_home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class PageHome extends StatefulWidget {
+  const PageHome({Key? key}) : super(key: key);
+
+  @override
+  _PageHomeState createState() => _PageHomeState();
+}
+
+class _PageHomeState extends State<PageHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Météo"),
+      ),
+      body: Center(),
+    );
+  }
+}
